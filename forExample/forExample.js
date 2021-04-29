cb894a322c42e393ffbf89fd71944f339b41470e
PankajiBirds <pankajgoyal.ibirdscollege@gmail.com>
2021-04-29 18:36:16 +0530

    29-04-2021

---
+++ forExample/forExample.js
@@ -0,0 +1,9 @@
+import { LightningElement } from 'lwc';
+
+export default class ForExample extends LightningElement {
+    studentName = ["Amita","Abhishek","Kunal","Hardik","Ankita","Anuj"];
+
+    employeeName = "Abhinav";
+    employeeEmail = "abhinav.k@yahoo.com";
+    employeeContact = "";
+}
---
+++ helloWorld/helloWorld.js
@@ -0,0 +1,7 @@
+import { LightningElement } from 'lwc';
+
+export default class HelloWorld extends LightningElement {
+    username = "Amit Sharma";
+    email = "amit.s@gmail.com";
+    contact = "7891089874";
+}
